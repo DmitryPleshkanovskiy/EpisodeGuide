6d15ec31a750977412aa095cefa58268f1b80f58
Dmitry <dmitry.pleshkanovskiy@gmail.com>
2024-03-03 12:18:24 +0100

    chore: tailwind classes sorting prettier setup

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,11 +7,11 @@ const route = useRoute()
 
 <template>
   <nav
-    class="w-full flex justify-center items-center p-2 shadow-md fixed bg-black z-10 scroll-m-1 bg-red"
+    class="bg-red fixed z-10 flex w-full scroll-m-1 items-center justify-center bg-black p-2 shadow-md"
   >
     <RouterLink
       v-if="route.path.includes('show')"
-      class="absolute text-white left-5"
+      class="absolute left-5 text-white"
       to="/"
       data-testid="back-to-main-link"
       aria-label="Back to main page"
@@ -20,7 +20,7 @@ const route = useRoute()
       <IconAngleLeft />
     </RouterLink>
     <RouterLink to="/">
-      <h1 class="flex gap-2 text-white text-3xl font-lobster">
+      <h1 class="flex gap-2 font-lobster text-3xl text-white">
         Episode <img class="h-10" src="/logo_64x64.png" alt="" /> Guide
       </h1>
     </RouterLink>
